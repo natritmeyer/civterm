d9f1f508f32ed7fdcee1250698d97202290b4ce4
Nat Ritmeyer <217265+natritmeyer@users.noreply.github.com>
2026-08-28 09:36:11 +0100

    linking city improvements to require advancements

--- src/model/cities/city_improvement.rs
+++ src/model/cities/city_improvement.rs
@@ -1,3 +1,5 @@
+use crate::model::advancements::Advancement;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum CityImprovement {
     Aqueduct,
@@ -7,11 +9,76 @@ pub enum CityImprovement {
     CityWalls,
     Colosseum,
     Courthouse,
-    Factory,
     Granary,
     Library,
     Marketplace,
     Palace,
     Temple,
     University,
+}
+
+impl CityImprovement {
+    pub fn required_advancement(&self) -> Option<Advancement> {
+        match self {
+            CityImprovement::Aqueduct => Some(Advancement::Construction),
+            CityImprovement::Bank => Some(Advancement::Banking),
+            CityImprovement::Barracks => None,
+            CityImprovement::Cathedral => Some(Advancement::Religion),
+            CityImprovement::CityWalls => Some(Advancement::Masonry),
+            CityImprovement::Colosseum => Some(Advancement::Construction),
+            CityImprovement::Courthouse => Some(Advancement::CodeOfLaws),
+            CityImprovement::Granary => Some(Advancement::Pottery),
+            CityImprovement::Library => Some(Advancement::Writing),
+            CityImprovement::Marketplace => Some(Advancement::Currency),
+            CityImprovement::Palace => Some(Advancement::Masonry),
+            CityImprovement::Temple => Some(Advancement::CeremonialBurial),
+            CityImprovement::University => Some(Advancement::University),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn improvements_require_their_advance() {
+        assert_eq!(
+            CityImprovement::Aqueduct.required_advancement(),
+            Some(Advancement::Construction)
+        );
+        assert_eq!(
+            CityImprovement::Bank.required_advancement(),
+            Some(Advancement::Banking)
+        );
+        assert_eq!(
+            CityImprovement::Cathedral.required_advancement(),
+            Some(Advancement::Religion)
+        );
+        assert_eq!(
+            CityImprovement::CityWalls.required_advancement(),
+            Some(Advancement::Masonry)
+        );
+        assert_eq!(
+            CityImprovement::Granary.required_advancement(),
+            Some(Advancement::Pottery)
+        );
+        assert_eq!(
+            CityImprovement::Library.required_advancement(),
+            Some(Advancement::Writing)
+        );
+        assert_eq!(
+            CityImprovement::Temple.required_advancement(),
+            Some(Advancement::CeremonialBurial)
+        );
+        assert_eq!(
+            CityImprovement::University.required_advancement(),
+            Some(Advancement::University)
+        );
+    }
+
+    #[test]
+    fn improvements_without_a_required_advancement() {
+        assert_eq!(CityImprovement::Barracks.required_advancement(), None);
+    }
 }
